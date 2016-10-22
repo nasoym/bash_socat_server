59303f4886f258cfbc79ff45eb3898d8f3e58631
Sinan Goo <sinan.goo@hypoport.de>
2016-10-22 09:23:44 +0200

    add some command line flags

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+while getopts "ds" OPTIONS; do case $OPTIONS in
+  d) VERBOSE_OPTIONS="-vv" ;;
+  s) SHORT="1" ;;
+  *) exit 1 ;;
+esac; done; shift $(( OPTIND - 1 ))
+
 : ${PORT:="1234"}
 : ${SERVICE:="./service.sh"}
+: ${VERBOSE_OPTIONS:=""}
 
 socat \
-  -vv \
+  $VERBOSE_OPTIONS \
   TCP-LISTEN:${PORT},crlf,reuseaddr,fork \
   EXEC:"${SERVICE}"
