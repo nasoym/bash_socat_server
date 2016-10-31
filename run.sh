4065513b77b21516e62552b7374414ef83d0e7f2
Sinan Goo <sinan.goo@hypoport.de>
2016-10-31 10:47:43 +0100

    improve argument handling

--- run.sh
+++ run.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-while getopts "d:p:r:v" OPTIONS; do case $OPTIONS in
+while getopts "d:p:r:vs" OPTIONS; do case $OPTIONS in
   v) VERBOSE_OPTIONS="-vv" ;;
+  s) RUN_AS_NOBODY=1 ;;
   p) PORT="$OPTARG" ;;
-  r) ROUTES_PATH=" -r $OPTARG" ;;
-  d) DEFAULT_ROUTE_HANDLER="-d $OPTARG" ;;
+  r) ROUTES_PATH="$OPTARG" ;;
+  d) DEFAULT_ROUTE_HANDLER="$OPTARG" ;;
   *) exit 1 ;;
 esac; done; shift $(( OPTIND - 1 ))
 
 : ${PORT:="8080"}
 : ${VERBOSE_OPTIONS:=""}
 : ${SERVICE:="$(dirname $0)/service.sh"}
-: ${ROUTES_PATH:=" -r $(dirname $0)/example_handlers"}
-: ${DEFAULT_ROUTE_HANDLER:=" -d $(dirname $0)/example_handlers/default"}
+
+: ${ROUTES_PATH:="$(dirname $0)/example_handlers"}
+: ${DEFAULT_ROUTE_HANDLER:="${ROUTES_PATH}/default"}
+
+: ${ROUTES_PATH_ARGUMENT:=" -r $ROUTES_PATH"}
+: ${DEFAULT_ROUTE_HANDLER_ARGUMENT:=" -d $DEFAULT_ROUTE_HANDLER"}
+
+if [[ "$RUN_AS_NOBODY" -eq 1 ]];then
+  SOCAT_SECURITY_ARGUMENT=",su=nobody" 
+fi
 
 socat \
   $VERBOSE_OPTIONS \
-  TCP-LISTEN:${PORT},reuseaddr,fork,su=nobody \
-  EXEC:"${SERVICE} ${ROUTES_PATH} ${DEFAULT_ROUTE_HANDLER}"
+  TCP-LISTEN:${PORT},reuseaddr,fork${SOCAT_SECURITY_ARGUMENT} \
+  EXEC:"${SERVICE} ${ROUTES_PATH_ARGUMENT} ${DEFAULT_ROUTE_HANDLER_ARGUMENT}"
 
